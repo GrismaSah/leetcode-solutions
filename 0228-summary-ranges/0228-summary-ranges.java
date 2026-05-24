f621874005449c706d12adb9afae4a564f20ce2c
Gr1sm4 <204060239+GrismaSah@users.noreply.github.com>
2026-05-24 19:50:13 +0530

    Time: 3 ms (77.41%), Space: 43.1 MB (52.15%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+         List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while(i < nums.length){
+
+            int start = nums[i];
+
+            // Move while numbers are consecutive
+            while(i < nums.length-1 && nums[i]+1 == nums[i+1]){
+                i++;
+            }
+
+            int end = nums[i];
+
+            if(start == end){
+                result.add(String.valueOf(start));
+            }
+            else{
+                result.add(start + "->" + end);
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
