8c65db86fa8b14870095278dc7abe5568d4787bb
Gr1sm4 <grishmashah598@gmail.com>
2026-06-23 22:43:31 +0530

    Time: 9 ms (42.62%), Space: 41.8 MB (84.63%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,14 @@
 class Solution {
-    public static int getfib(int n){
-        if(n<2){
+    // public static int getfib(int n){
+    //     if(n<2){
+    //         return n;
+    //     }
+    //     return getfib(n-2)+ getfib(n-1);
+    // }
+    public int fib(int n) {
+           if(n<2){
             return n;
         }
-        return getfib(n-2)+ getfib(n-1);
-    }
-    public int fib(int n) {
-        return getfib(n);
+        return fib(n-2)+ fib(n-1);
     }
 }
