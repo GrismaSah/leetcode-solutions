ccb4386940a98d74202bb6132fe95de0a920f9eb
Gr1sm4 <grishmashah598@gmail.com>
2026-07-02 00:30:11 +0530

    Time: 2 ms (92.02%), Space: 47.9 MB (37.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-         int k = 0;
-
-        // put non-zero elements at front
-        for(int i=0;i<nums.length;i++){
-
-            if(nums[i] != 0){
-
-                nums[k] = nums[i];
-                k++;
+        int k =0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                nums[k++] = nums[i];
+                
+                
             }
         }
-
-        // fill remaining positions with 0
-        while(k < nums.length){
-
+        while(k<nums.length){
             nums[k] = 0;
-            k++;
-        }
+        k++;
+        } 
     }
 }
