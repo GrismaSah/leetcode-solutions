a97b117814038753bfec28496f670dd2b611afc0
Grizzy <grishmashah598@gmail.com>
2026-08-09 14:34:56 +0530

    Time: 1 ms (99.88%), Space: 55.7 MB (32.38%) - LeetHub - Time: O(n), Space: O(1)

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            int count =1;
-            for(int j=i+1; j<n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            if(count > n/2){
-                int ans =  nums[i];
-                return ans;
-            }
+       int majority = nums[0], vote = 1;
+       for(int i=1; i<nums.length; i++){
+        if(nums[i] == majority){
+            vote++;
         }
-        return -1;
+        else if( vote == 0){
+            vote++;
+            majority = nums[i];
+
+        }
+        else{
+            vote--;
+        }
+       }
+       return majority;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
