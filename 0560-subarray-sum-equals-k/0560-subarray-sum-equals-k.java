3d638cadc1934a6a99e5c05b314890f876c554cb
Grizzy <grishmashah598@gmail.com>
2026-08-20 00:30:27 +0530

    Time: 25 ms (46.3%), Space: 49 MB (34.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,23 +1,23 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-         HashMap<Integer, Integer> map = new HashMap<>();
+       HashMap<Integer, Integer> map = new HashMap<>();
+       map.put(0, 1);
 
-        map.put(0, 1);
-
-        int sum = 0;
-        int count = 0;
-
-        for(int num : nums) {
-
-            sum += num;
-
-            if(map.containsKey(sum - k)) {
-                count += map.get(sum - k);
-            }
-
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+       int prefixSum  = 0; 
+       int result = 0;
+       for(int num : nums){
+        prefixSum += num;
+        if(map.containsKey(prefixSum - k)){
+            result += map.get(prefixSum - k);
         }
-
-        return count;
+        
+        map.put(prefixSum, map.getOrDefault(prefixSum, 0)+1);
+        
+       }
+       return result;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
