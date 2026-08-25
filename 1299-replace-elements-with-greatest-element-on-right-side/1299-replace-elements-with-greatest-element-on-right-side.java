1ac1416175bcc8416ac2ea885d491d835cd89646
Grizzy <grishmashah598@gmail.com>
2026-08-25 23:08:21 +0530

    Time: 3 ms (69.51%), Space: 49.4 MB (23.34%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int[] replaceElements(int[] arr) {
+    public int[] replaceElements(int[] nums) {
         int max = -1;
 
-        for(int i = arr.length-1; i>=0; i--){
-
-            int current = arr[i];
-
-            arr[i] = max;
-
-            max = Math.max(max,current);
+        for(int i = nums.length - 1; i>=0; i--){
+            int curr = nums[i];
+            nums[i] = max;
+            max = Math.max(max, curr);
+           
         }
-
-        return arr;
+        return nums;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
