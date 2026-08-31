012564cc62ad33070034edff78950ff906307455
Grizzy <grishmashah598@gmail.com>
2026-08-31 22:23:26 +0530

    Time: 4 ms (25.79%), Space: 269 MB (5.21%) - LeetHub - Time: O(n), Space: O(1)

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,15 +1,15 @@
 class Solution {
-
-    public static void reverse(int[] nums, int start, int end){
-        while(start<=end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-
+    public void reverse(int[] nums, int start, int end){
+        while(start <= end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
         }
     }
+
+    
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
@@ -18,3 +18,7 @@ class Solution {
         reverse(nums, k, n-1);
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
