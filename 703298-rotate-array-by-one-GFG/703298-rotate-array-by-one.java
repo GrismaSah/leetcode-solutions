bd4736c9f9fc8209db20eecf531d0c4849557cc2
Grizzy <grishmashah598@gmail.com>
2026-08-31 21:54:29 +0530

    Force committed solution - Time: O(n), Space: O(1)

--- 703298-rotate-array-by-one-GFG/703298-rotate-array-by-one.java
+++ 703298-rotate-array-by-one-GFG/703298-rotate-array-by-one.java
@@ -1,12 +1,20 @@
 class Solution {
-    public void rotate(int[] arr) {
-        int n = arr.length;
-        int k = n-1;
-        int last = arr[n-1];
-        for(int i=n-2; i>=0; i--){
-            arr[k--] = arr[i];
+    public void rotate(int[] nums) {
+        // int temp = nums[0];
+        // int k=0;
+        // for(int i=1; i<nums.length; i++){
+        //     nums[k] = nums[i];
+        //     k++;
+        // }
+        // nums[k] = temp; 
+        
+        int temp = nums[nums.length-1];
+        int k =nums.length-1;
+        for(int i = nums.length-2; i>=0; i--){
+            nums[k] = nums[i];
+            k--;
         }
-        arr[0] = last;
+        nums[0] = temp;
     }
 }
 
