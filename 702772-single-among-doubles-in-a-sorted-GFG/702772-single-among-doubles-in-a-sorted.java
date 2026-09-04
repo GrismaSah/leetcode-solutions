a42686860e4cd9ec8df02470d1edd3e11db3ef0f
Grizzy <grishmashah598@gmail.com>
2026-09-04 23:56:01 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702772-single-among-doubles-in-a-sorted-GFG/702772-single-among-doubles-in-a-sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    int single(int[] nums) {
+       int num = 0;
+       for(int i=0; i<nums.length; i++){
+           num = num ^ nums[i];
+       }
+       return num;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
