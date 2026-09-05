39079c23a991b023f10d476fd3c3edc3dbefdfa7
Grizzy <grishmashah598@gmail.com>
2026-09-05 09:25:06 +0530

    Added solution - Time: O(n), Space: O(n)

--- 703043-longest-subarray-with-sum-k-GFG/703043-longest-subarray-with-sum-k.java
+++ 703043-longest-subarray-with-sum-k-GFG/703043-longest-subarray-with-sum-k.java
@@ -1,20 +1,22 @@
 class Solution {
     public int longestSubarray(int[] nums, int k) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        int prefixSum = 0; 
-        int len = 0;
-        
+        HashMap<Integer, Integer> map = new HashMap <>();
+        int pSum = 0, len = 0;
+         
         map.put(0, -1);
+        
         for(int i=0; i<nums.length; i++){
-            prefixSum += nums[i];
-            if(map.containsKey(prefixSum -k)){
-                len = Math.max(len, i - map.get(prefixSum - k));
+            pSum += nums[i];
+            if(map.containsKey(pSum - k)){
+                len = Math.max(len, i - map.get(pSum -k));
             }
-            if(!map.containsKey(prefixSum)){
-                map.put(prefixSum, i);
+            if(!map.containsKey(pSum)){
+                map.put(pSum, i);
             }
         }
         return len;
+         
+         
         
     }
 }
