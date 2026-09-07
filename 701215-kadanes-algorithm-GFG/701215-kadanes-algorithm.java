27c32dfc5157b085cc194ecfd5b5c15bfebd1ec4
Grizzy <grishmashah598@gmail.com>
2026-09-07 22:14:27 +0530

    Added solution - Time: O(n), Space: O(1)

--- 701215-kadanes-algorithm-GFG/701215-kadanes-algorithm.java
+++ 701215-kadanes-algorithm-GFG/701215-kadanes-algorithm.java
@@ -1,13 +1,13 @@
 class Solution {
     int maxSubarraySum(int[] nums) {
-        int max = Integer.MIN_VALUE;
-                int sum = 0;
-                for(int i=0; i<nums.length; i++){
-                    sum += nums[i];
-                    if(sum > max ) max = sum;
-                    if(sum < 0) sum = 0;
-                }
-                return max;
+        int max = Integer.MIN_VALUE, sum =0;
+        for(int i = 0; i<nums.length; i++){
+            sum += nums[i];
+            if(sum > max) max = sum;
+            if(sum < 0 ) sum = 0;
+            
+        }
+        return max;
     }
 }
 
